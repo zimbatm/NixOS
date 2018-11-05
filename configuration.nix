a88d2af77fcd47bdeb858b0f9fa630978f522c23
R-VdP <R-VdP@users.noreply.github.com>
2018-11-05 11:58:58 +0100

    clearer messages from the reboot service.

--- configuration.nix
+++ configuration.nix
@@ -244,12 +244,13 @@
         # Current system is the most recently activated system, but it's kernel only gets loaded after a reboot.
         # Booted system is the system that we booted in, and whose kernel is thus currently loaded.
         ExecStart = ''${pkgs.bash}/bin/bash -c\
-          "if [ $(dirname $(readlink /run/current-system/kernel)) != $(dirname $(readlink /run/booted-system/kernel)) ] &&\
-              [ $(date +%%s) -lt $(date --date=\"$(date --date=\'today\' +%%Y-%%m-%%d) + 5 hours\" +%%s) ]; then\
+          "if [ $(dirname $(readlink /run/current-system/kernel)) = $(dirname $(readlink /run/booted-system/kernel)) ]; then\
+            echo No reboot required.;\
+          elif [ $(date +%%s) -gt $(date --date=\"$(date --date=\'today\' +%%Y-%%m-%%d) + 5 hours\" +%%s) ]; then\
+            echo Time window for reboot has passed.;\
+          else\
             echo Rebooting...;\
             systemctl --no-block reboot;\
-          else\
-            echo No reboot required.;\
           fi"
         '';
       };
