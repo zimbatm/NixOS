bc5d5a14e254477fffaa5afd9b00ecb2811d02bb
R-VdP <R-VdP@users.noreply.github.com>
2019-02-27 09:54:46 +0000

    Enable garbage collection for the docker-registry.

--- docker-registry.nix
+++ docker-registry.nix
@@ -16,6 +16,8 @@
     enable = true;
     listenAddress = "127.0.0.1";
     port = 5000;
+    enableGarbageCollect = true;
+    enableDelete = true;
     storagePath = "/opt/docker-registry";
   };
 
