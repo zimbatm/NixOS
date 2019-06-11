98a79caea8a0e3c57033eae68b7d3499a7b0fb7a
R-VdP <R-VdP@users.noreply.github.com>
2019-06-11 14:17:21 +0000

    Adjust the docker internal IP ranges.

--- docker.nix
+++ docker.nix
@@ -32,18 +32,36 @@
   environment.etc.docker_conf = {
     enable = true;
     target = "docker/daemon.json";
+
+    # Docker internal IP addressing
+    # Ranges used: 172.28.0.0/16, 172.29.0.0/16
+    #
+    # Docker bridge
+    # 172.28.0.1/18
+    #   -> 2^14 - 2 (16382) hosts 172.28.0.1 -> 172.28.127.254
+    #
+    # Custom networks (448 networks in total)
+    # 172.28.64.0/18 in /24 blocks
+    #   -> 2^6 (64) networks 172.28.64.0/24 -> 172.28.127.0/24
+    # 172.28.128.0/17 in /24 blocks
+    #   -> 2^7 (128) networks 172.28.128.0/24 -> 172.28.255.0/24
+    # 172.29.0.0/16 in /24 blocks
+    #   -> 2^8 (256) networks 172.29.0.0/24 -> 172.29.255.0/24
+
     text = ''
       {
-        "bip": "10.231.0.1/24",
+        "bip": "172.28.0.1/18",
         "default-address-pools": [
           {
-            "scope": "local",
-            "base": "10.232.0.0/16",
+            "base": "172.28.64.0/18",
             "size": 24
           },
           {
-            "scope": "global",
-            "base": "10.233.0.0/16",
+            "base": "172.28.128.0/17",
+            "size": 24
+          },
+          {
+            "base": "172.29.0.0/16",
             "size": 24
           }
         ]
