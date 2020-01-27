383200988d174a6140fbb00b827c13fefd6fdea7
R-VdP <R-VdP@users.noreply.github.com>
2020-01-27 16:35:59 +0100

    Set the nix channel during activation, the channel can be configured through a new setting.

--- modules/system.nix
+++ modules/system.nix
@@ -13,142 +13,160 @@
 with lib;
 
 {
-  zramSwap = {
-    enable = true;
-    algorithm = "zstd";
-    memoryPercent = 40;
+  options = {
+    settings.system.nix_channel = mkOption {
+      type = types.str;
+      default = "19.09";
+    };
   };
 
-  security = {
-    sudo = {
+  config = {
+    zramSwap = {
       enable = true;
-      wheelNeedsPassword = false;
-    };
-    pam.services.su.forwardXAuth = mkForce false;
-  };
-  
-  environment = {
-    # See https://nixos.org/nix/manual/#ssec-values for documentation on escaping ${
-    shellInit = ''
-      if [ "''${TERM}" != "screen" ] || [ -z "''${TMUX}" ]; then
-        alias nixos-rebuild='printf "Please run nixos-rebuild only from within a tmux session.\c" 2> /dev/null'
-      fi
-    '';
-    shellAliases = {
-      vi = "vim";
-      # Have bash resolve aliases with sudo (https://askubuntu.com/questions/22037/aliases-not-available-when-using-sudo)
-      sudo = "sudo ";
-      whereami = "curl ipinfo.io";
-    };
-  };
-  
-  system.activationScripts = {
-    tunnel_key_permissions = ''
-      for FILE in "/etc/nixos/local/id_tunnel" "/etc/nixos/local/id_tunnel.pub"; do
-        if [ -f ''${FILE} ]; then
-          chown root:root ''${FILE}
-          chmod 0400 ''${FILE}
-        fi
-      done
-    '';
-    settings_link = let
-      hostname = config.networking.hostName;
-      settings_path = "/etc/nixos/settings.nix";
-    in ''
-      if [ $(realpath ${settings_path}) != "/etc/nixos/hosts/${hostname}.nix" ]; then
-        ln --force --symbolic hosts/${hostname}.nix ${settings_path}
-      fi
-    '';
-  };
-
-  # No fonts needed on a headless system
-  fonts.fontconfig.enable = mkForce false;
-
-  programs = {
-    bash.enableCompletion = true;
-
-    ssh = {
-      startAgent = false;
-      # We do not have GUIs
-      setXAuthLocation = false;
+      algorithm = "zstd";
+      memoryPercent = 40;
     };
 
-    tmux = {
-      enable = true;
-      newSession = true;
-      clock24 = true;
-      historyLimit = 10000;
-      extraTmuxConf = ''
-        set -g mouse on
-      '';
-    };
-  };
-
-  services = {
-    fstrim.enable = true;
-    # Avoid pulling in unneeded dependencies
-    udisks2.enable = false;
-
-    timesyncd = {
-      enable = true;
-      servers = mkDefault [ 
-        "0.nixos.pool.ntp.org"
-        "1.nixos.pool.ntp.org"
-        "2.nixos.pool.ntp.org"
-        "3.nixos.pool.ntp.org"
-        "time.windows.com" 
-        "time.google.com"
-      ];
-    };
-
-    htpdate = {
-      enable = true;
-      servers = [ "www.kernel.org" "www.google.com" "www.cloudflare.com" ];
-    };
-
-    journald = {
-      rateLimitBurst = 1000;
-      rateLimitInterval = "5s";
-      extraConfig = ''
-        Storage=persistent
-      '';
-    };
-
-    # See man logind.conf
-    logind = {
-      extraConfig = ''
-        HandlePowerKey=poweroff
-        PowerKeyIgnoreInhibited=yes
-      '';
-    };
-    
-    avahi = {
-      enable  = true;
-      nssmdns = true;
-      extraServiceFiles = {
-        ssh = "${pkgs.avahi}/etc/avahi/services/ssh.service";
-      };
-      publish = {
+    security = {
+      sudo = {
         enable = true;
-        domain = true;
-        addresses   = true;
-        workstation = true;
+        wheelNeedsPassword = false;
+      };
+      pam.services.su.forwardXAuth = mkForce false;
+    };
+
+    environment = {
+      # See https://nixos.org/nix/manual/#ssec-values for documentation on escaping ${
+      shellInit = ''
+        if [ "''${TERM}" != "screen" ] || [ -z "''${TMUX}" ]; then
+          alias nixos-rebuild='printf "Please run nixos-rebuild only from within a tmux session.\c" 2> /dev/null'
+        fi
+      '';
+      shellAliases = {
+        vi = "vim";
+        # Have bash resolve aliases with sudo (https://askubuntu.com/questions/22037/aliases-not-available-when-using-sudo)
+        sudo = "sudo ";
+        whereami = "curl ipinfo.io";
       };
     };
-  };
 
-  hardware = {
-    enableRedistributableFirmware = true;
-    cpu.intel.updateMicrocode = true;
-    cpu.amd.updateMicrocode   = true;
-  };
+    system.activationScripts = {
+      tunnel_key_permissions = ''
+        for FILE in "/etc/nixos/local/id_tunnel" "/etc/nixos/local/id_tunnel.pub"; do
+          if [ -f ''${FILE} ]; then
+            chown root:root ''${FILE}
+            chmod 0400 ''${FILE}
+          fi
+        done
+      '';
+      settings_link = let
+        hostname = config.networking.hostName;
+        settings_path = "/etc/nixos/settings.nix";
+      in ''
+        if [ $(realpath ${settings_path}) != "/etc/nixos/hosts/${hostname}.nix" ]; then
+          ln --force --symbolic hosts/${hostname}.nix ${settings_path}
+        fi
+      '';
+      nix_channel_msf = {
+        text = ''
+          # We override the root nix channel with the one defined by settings.system.nix_channel
+          echo "https://nixos.org/channels/nixos-${config.settings.system.nix_channel} nixos" > "/root/.nix-channels"
+        '';
+        # We overwrite the value set by the default NixOS activation snippet, that snippet should have run first
+        # so that the additional initialisation has been performed.
+        # See /run/current-system/activate for the currently defined snippets.
+        deps = [ "nix" ];
+      };
+    };
 
-  documentation = {
-    man.enable  = true;
-    doc.enable  = false;
-    dev.enable  = false;
-    info.enable = false;
-  };
+    # No fonts needed on a headless system
+    fonts.fontconfig.enable = mkForce false;
 
+    programs = {
+      bash.enableCompletion = true;
+
+      ssh = {
+        startAgent = false;
+        # We do not have GUIs
+        setXAuthLocation = false;
+      };
+
+      tmux = {
+        enable = true;
+        newSession = true;
+        clock24 = true;
+        historyLimit = 10000;
+        extraTmuxConf = ''
+          set -g mouse on
+        '';
+      };
+    };
+
+    services = {
+      fstrim.enable = true;
+      # Avoid pulling in unneeded dependencies
+      udisks2.enable = false;
+
+      timesyncd = {
+        enable = true;
+        servers = mkDefault [
+          "0.nixos.pool.ntp.org"
+          "1.nixos.pool.ntp.org"
+          "2.nixos.pool.ntp.org"
+          "3.nixos.pool.ntp.org"
+          "time.windows.com"
+          "time.google.com"
+        ];
+      };
+
+      htpdate = {
+        enable = true;
+        servers = [ "www.kernel.org" "www.google.com" "www.cloudflare.com" ];
+      };
+
+      journald = {
+        rateLimitBurst = 1000;
+        rateLimitInterval = "5s";
+        extraConfig = ''
+          Storage=persistent
+        '';
+      };
+
+      # See man logind.conf
+      logind = {
+        extraConfig = ''
+          HandlePowerKey=poweroff
+          PowerKeyIgnoreInhibited=yes
+        '';
+      };
+
+      avahi = {
+        enable  = true;
+        nssmdns = true;
+        extraServiceFiles = {
+          ssh = "${pkgs.avahi}/etc/avahi/services/ssh.service";
+        };
+        publish = {
+          enable = true;
+          domain = true;
+          addresses   = true;
+          workstation = true;
+        };
+      };
+    };
+
+    hardware = {
+      enableRedistributableFirmware = true;
+      cpu.intel.updateMicrocode = true;
+      cpu.amd.updateMicrocode   = true;
+    };
+
+    documentation = {
+      man.enable  = true;
+      doc.enable  = false;
+      dev.enable  = false;
+      info.enable = false;
+    };
+  };
 }
 
