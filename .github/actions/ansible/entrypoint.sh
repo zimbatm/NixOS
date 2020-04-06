37d8261f989a7634ddf3849f924d51201523aaaf
R-VdP <R-VdP@users.noreply.github.com>
2020-04-06 12:29:07 +0200

    Add a github action to automatically deploy to the relays.

---
+++ .github/actions/ansible/entrypoint.sh
@@ -0,0 +1,14 @@
+#! /bin/sh
+
+umask 0077
+
+echo "${VAULT_PASS}" > ~/.vault_pass
+ansible-vault view --vault-password-file=~/.vault_pass ansible/id_ec_robot.secret > ~/id_ec
+chmod 0600 ~/id_ec
+
+ansible-playbook --key-file "~/id_ec" \
+                 --vault-password-file ~/.vault_pass \
+                 --inventory ansible/hosts.yml \
+                 --extra-vars "build_sha=$GITHUB_SHA" \
+                 ansible/deploy.yml
+
