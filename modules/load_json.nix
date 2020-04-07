eb3c77c79678f58fb029b089b9ec7dead2f57f62
R-VdP <r-vdp@users.noreply.github.com>
2020-04-07 02:21:55 +0200

    Load the user settings from a single JSON file.

--- modules/load_json.nix
+++ modules/load_json.nix
@@ -20,24 +20,23 @@ with (import ../msf_lib.nix { inherit lib; });
 {
   config = {
     settings.users.users = let
-      host_name        = config.settings.network.host_name;
-      tunnel_only_path = ../org-spec/json/tunnel_only.json;
-      host_users_path  = ../org-spec/json + "/${host_name}/users.json";
+      host_name       = config.settings.network.host_name;
+      users_json_path = ../org-spec/json/users.json;
+      json_data = builtins.fromJSON (builtins.readFile users_json_path);
 
-      # Load a JSON property from the given JSON file, the property should contain a list
-      # which will be converted to an attribute set with every value set to a given constant
-      # A JSON list like
-      #   { "enable": [ "foo", "bar" ] }
-      # will be transformed into an attribute set, e.g.
+      # Load the list at path in the given attribute set and convert it to
+      # an attribute set with every list element as a key and the value
+      # set to a given constant.
+      # Am attribute set like
+      #   { per-host.benuc002.enable: [ "foo", "bar" ] }
+      # will be transformed into an attribute set like
       #   { foo = const; bar = const; }
-      load_json_const = json_path: json_property: value: mkIf (builtins.pathExists json_path) (
-        listToAttrs (map (user: nameValuePair user value)
-                         (builtins.fromJSON (builtins.readFile json_path))."${json_property}")
-      );
+      listToAttrs_const = attrset: path: const: listToAttrs (map (name: nameValuePair name const)
+                                                                 (attrByPath path [] attrset));
     in
       # recursiveUpdate merges the two resulting attribute sets recursively
-      recursiveUpdate (load_json_const tunnel_only_path "tunnel_only" msf_lib.user_roles.tunnelOnly)
-                      (load_json_const host_users_path  "enable"      { enable = true; });
+      recursiveUpdate (listToAttrs_const json_data [ "tunnel_only" ]                      msf_lib.user_roles.tunnelOnly)
+                      (listToAttrs_const json_data [ "per-host" "${host_name}" "enable" ] { enable = true; });
   };
 }
 
