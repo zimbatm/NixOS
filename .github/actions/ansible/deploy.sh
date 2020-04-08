12cc06746b32b307fada0e1c97bd9446102477ed
R-VdP <R-VdP@users.noreply.github.com>
2020-04-08 23:28:33 +0200

    Use the new script to build the inventory dynamically based upon the commits.

--- .github/actions/ansible/deploy.sh
+++ .github/actions/ansible/deploy.sh
@@ -6,7 +6,7 @@ echo "${VAULT_PASS}" > /root/.vault_pass
 ansible-vault view --vault-password-file=/root/.vault_pass ansible/id_ec_robot.secret > /root/.id_ec
 chmod 0600 /root/.id_ec
 
-cat "${GITHUB_EVENT_PATH}"
+python3 ansible/build_inventory.py --eventlog "${GITHUB_EVENT_PATH}" > ansible/hosts.yaml
 
 ansible-playbook --timeout=30 \
                  --key-file "/root/.id_ec" \
