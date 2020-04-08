99a1f5a72127bc1fe6d2dba0c467524e0a2659bb
R-VdP <141248+R-VdP@users.noreply.github.com>
2020-04-08 14:03:07 +0000

    Update deploy.sh

--- .github/actions/ansible/deploy.sh
+++ .github/actions/ansible/deploy.sh
@@ -2,15 +2,15 @@
 
 umask 0077
 
-echo "${VAULT_PASS}" > ~/.vault_pass
-ansible-vault view --vault-password-file=~/.vault_pass ansible/id_ec_robot.secret > ~/.id_ec
-chmod 0600 ~/.id_ec
+echo "${VAULT_PASS}" > /root/.vault_pass
+ansible-vault view --vault-password-file=~/.vault_pass ansible/id_ec_robot.secret > /root/.id_ec
+chmod 0600 /root/.id_ec
 
 git log --format=%B --max-count=1 "${GITHUB_SHA}" | egrep --only-matching "(x-nixos:rebuild:relay_port:[1-9][0-9]*)"
 
 ansible-playbook --timeout=30 \
-                 --key-file "~/.id_ec" \
-                 --vault-password-file ~/.vault_pass \
+                 --key-file "/root/.id_ec" \
+                 --vault-password-file /root/.vault_pass \
                  --inventory ansible/hosts.yml \
                  --extra-vars "build_sha=${GITHUB_SHA}" \
                  ansible/deploy.yml
