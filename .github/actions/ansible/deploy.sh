2a4dc7f2955a507ef2e4898821094ce9309ee925
R-VdP <141248+R-VdP@users.noreply.github.com>
2020-04-08 13:42:18 +0000

    Update deploy.sh

--- .github/actions/ansible/deploy.sh
+++ .github/actions/ansible/deploy.sh
@@ -8,7 +8,7 @@ chmod 0600 ~/.id_ec
 
 git log --format=%B --max-count=1 "${GITHUB_SHA}" | egrep --only-matching "(x-nixos:rebuild:relay_port:[1-9][0-9]*)"
 
-ansible-playbook -vvv
+ansible-playbook -vvv \
                  --timeout=30 \
                  --key-file "~/.id_ec" \
                  --vault-password-file ~/.vault_pass \
