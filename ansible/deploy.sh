8f799267e2ff52ec11880f4a815d6aba48b1c64f
R-VdP <R-VdP@users.noreply.github.com>
2020-04-09 00:29:04 +0200

    Pass the connection time out as a variable to the script that builds the inventory.
    (x-nixos:rebuild:relay_port:6002)
    (x-nixos:rebuild:relay_port:6020)
    (x-nixos:rebuild:relay_port:9002)

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -10,8 +10,8 @@ echo "${VAULT_PASS}" > "${VAULTPASS}"
 ansible-vault view --vault-password-file="${VAULTPASS}" ansible/id_ec_robot.secret > "${KEYFILE}"
 chmod 0600 "${KEYFILE}"
 
-python3 ansible/build_inventory.py --keyfile "${KEYFILE}"
-                                   --timeout "${CONNECTION_TIMEOUT}"
+python3 ansible/build_inventory.py --keyfile "${KEYFILE}" \
+                                   --timeout "${CONNECTION_TIMEOUT}" \
                                    --eventlog "${GITHUB_EVENT_PATH}" > ansible/hosts.yml
 
 ansible-playbook --timeout="${CONNECTION_TIMEOUT}" \
