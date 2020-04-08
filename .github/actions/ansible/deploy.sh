90e748a95ba57f6a5d69b30a5c50e8a4fc7ba903
R-VdP <R-VdP@users.noreply.github.com>
2020-04-08 13:35:44 +0200

    Add git to the ansible container.

--- .github/actions/ansible/deploy.sh
+++ .github/actions/ansible/deploy.sh
@@ -3,12 +3,12 @@
 umask 0077
 
 echo "${VAULT_PASS}" > ~/.vault_pass
-ansible-vault view --vault-password-file=~/.vault_pass ansible/id_ec_robot.secret > ~/id_ec
-chmod 0600 ~/id_ec
+ansible-vault view --vault-password-file=~/.vault_pass ansible/id_ec_robot.secret > ~/.id_ec
+chmod 0600 ~/.id_ec
 
-git log --format=%B -n 1 "${GITHUB_SHA}"
+git log --format=%B --max-count=1 "${GITHUB_SHA}" | egrep --only-matching "(x-nixos:rebuild:relay_port:[1-9][0-9]*)"
 
-ansible-playbook --key-file "~/id_ec" \
+ansible-playbook --key-file "~/.id_ec" \
                  --vault-password-file ~/.vault_pass \
                  --inventory ansible/hosts.yml \
                  --extra-vars "build_sha=${GITHUB_SHA}" \
