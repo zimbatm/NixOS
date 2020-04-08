f41f8937019fa962c72250cc1f1676a972cc37ce
R-VdP <141248+R-VdP@users.noreply.github.com>
2020-04-08 13:37:36 +0000

    Increase the ansible connection timeout.

--- .github/actions/ansible/deploy.sh
+++ .github/actions/ansible/deploy.sh
@@ -8,7 +8,8 @@ chmod 0600 ~/.id_ec
 
 git log --format=%B --max-count=1 "${GITHUB_SHA}" | egrep --only-matching "(x-nixos:rebuild:relay_port:[1-9][0-9]*)"
 
-ansible-playbook --key-file "~/.id_ec" \
+ansible-playbook --timeout=30 \
+                 --key-file "~/.id_ec" \
                  --vault-password-file ~/.vault_pass \
                  --inventory ansible/hosts.yml \
                  --extra-vars "build_sha=${GITHUB_SHA}" \
