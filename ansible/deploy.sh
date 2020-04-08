8018dae2b9ac95a8326a49588ccab7ee09821faf
R-VdP <R-VdP@users.noreply.github.com>
2020-04-09 00:22:39 +0200

    Make the key file into a variable for the inventory build script.

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -2,15 +2,18 @@
 
 umask 0077
 
-echo "${VAULT_PASS}" > /root/.vault_pass
-ansible-vault view --vault-password-file=/root/.vault_pass ansible/id_ec_robot.secret > /root/.id_ec
-chmod 0600 /root/.id_ec
+KEYFILE="/root/.id_ec"
+VAULTPASS="/root/.vault_pass"
 
-python3 ansible/build_inventory.py --eventlog "${GITHUB_EVENT_PATH}" > ansible/hosts.yml
+echo "${VAULT_PASS}" > "${VAULTPASS}"
+ansible-vault view --vault-password-file="${VAULTPASS}" ansible/id_ec_robot.secret > "${KEYFILE}"
+chmod 0600 "${KEYFILE}"
+
+python3 ansible/build_inventory.py --keyfile "${KEYFILE}" --eventlog "${GITHUB_EVENT_PATH}" > ansible/hosts.yml
 
 ansible-playbook --timeout=30 \
-                 --key-file "/root/.id_ec" \
-                 --vault-password-file /root/.vault_pass \
+                 --key-file "${KEYFILE}" \
+                 --vault-password-file "${VAULTPASS}" \
                  --inventory ansible/hosts.yml \
                  --extra-vars "build_sha=${GITHUB_SHA}" \
                  ansible/deploy.yml
