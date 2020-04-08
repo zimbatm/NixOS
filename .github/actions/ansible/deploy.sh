e6626f868a6ea110a605d02501a63138ed0b3555
R-VdP <R-VdP@users.noreply.github.com>
2020-04-08 23:30:33 +0200

    Typo.

--- .github/actions/ansible/deploy.sh
+++ .github/actions/ansible/deploy.sh
@@ -6,7 +6,7 @@ echo "${VAULT_PASS}" > /root/.vault_pass
 ansible-vault view --vault-password-file=/root/.vault_pass ansible/id_ec_robot.secret > /root/.id_ec
 chmod 0600 /root/.id_ec
 
-python3 ansible/build_inventory.py --eventlog "${GITHUB_EVENT_PATH}" > ansible/hosts.yaml
+python3 ansible/build_inventory.py --eventlog "${GITHUB_EVENT_PATH}" > ansible/hosts.yml
 
 ansible-playbook --timeout=30 \
                  --key-file "/root/.id_ec" \
