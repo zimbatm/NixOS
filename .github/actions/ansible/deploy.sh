b781a25826d2f45b6f35806fd2a3f44506a2451f
R-VdP <R-VdP@users.noreply.github.com>
2020-04-08 13:32:09 +0200

    Try to reach a gatewayed host via ansible.

--- .github/actions/ansible/deploy.sh
+++ .github/actions/ansible/deploy.sh
@@ -6,9 +6,11 @@ echo "${VAULT_PASS}" > ~/.vault_pass
 ansible-vault view --vault-password-file=~/.vault_pass ansible/id_ec_robot.secret > ~/id_ec
 chmod 0600 ~/id_ec
 
+git log --format=%B -n 1 "${GITHUB_SHA}"
+
 ansible-playbook --key-file "~/id_ec" \
                  --vault-password-file ~/.vault_pass \
                  --inventory ansible/hosts.yml \
-                 --extra-vars "build_sha=$GITHUB_SHA" \
+                 --extra-vars "build_sha=${GITHUB_SHA}" \
                  ansible/deploy.yml
 
