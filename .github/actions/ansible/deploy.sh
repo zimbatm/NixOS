0a431bd1e6fdd4c5d662ff25a42530896fd1e95c
R-VdP <141248+R-VdP@users.noreply.github.com>
2020-04-08 19:58:30 +0000

    Remove test command.
    And test an extended log message.

--- .github/actions/ansible/deploy.sh
+++ .github/actions/ansible/deploy.sh
@@ -6,7 +6,6 @@ echo "${VAULT_PASS}" > /root/.vault_pass
 ansible-vault view --vault-password-file=/root/.vault_pass ansible/id_ec_robot.secret > /root/.id_ec
 chmod 0600 /root/.id_ec
 
-git log --format=%B --max-count=1 "${GITHUB_SHA}" | egrep --only-matching "(x-nixos:rebuild:relay_port:[1-9][0-9]*)"
 cat "${GITHUB_EVENT_PATH}"
 
 ansible-playbook --timeout=30 \
