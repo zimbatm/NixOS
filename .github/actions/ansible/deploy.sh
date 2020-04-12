bfb5ca5983dd95aa3c6550a13e5e1cf28a654bc2
R-VdP <R-VdP@users.noreply.github.com>
2020-04-12 22:14:31 +0200

    Correct the path to the build_inventory.py script.

--- .github/actions/ansible/deploy.sh
+++ .github/actions/ansible/deploy.sh
@@ -2,19 +2,20 @@
 
 umask 0077
 
-ANSIBLE_DIR=".github/actions/ansible"
+# The workdir of the Docker container will be the clone of the NixOS repo
+ANSIBLE_DIR="./.github/actions/ansible"
 
 KEYFILE="/root/.id_ec"
 VAULTPASS="/root/.vault_pass"
 CONNECTION_TIMEOUT=120
 
 echo "${VAULT_PASS}" > "${VAULTPASS}"
-ansible-vault view --vault-password-file="${VAULTPASS}" "${ANSIBLE_DIR}/id_ec_robot.secret" > "${KEYFILE}"
+ansible-vault view --vault-password-file="${VAULTPASS}" "${ANSIBLE_DIR}"/id_ec_robot.secret > "${KEYFILE}"
 chmod 0400 "${KEYFILE}"
 
-python3 ansible/build_inventory.py --keyfile "${KEYFILE}" \
-                                   --timeout "${CONNECTION_TIMEOUT}" \
-                                   --eventlog "${GITHUB_EVENT_PATH}" > hosts.yml
+python3 "${ANSIBLE_DIR}"/build_inventory.py --keyfile "${KEYFILE}" \
+                                            --timeout "${CONNECTION_TIMEOUT}" \
+                                            --eventlog "${GITHUB_EVENT_PATH}" > hosts.yml
 
 export ANSIBLE_PYTHON_INTERPRETER="auto_silent"
 export ANSIBLE_HOST_KEY_CHECKING="False"
