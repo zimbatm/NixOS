2c198ee0f722dfc10ad59027d8db75102777ec59
R-VdP <141248+R-VdP@users.noreply.github.com>
2020-04-21 12:20:59 +0200

    Add a module for the nixos encryption manager.

---
+++ modules/encryption_manager.nix
@@ -0,0 +1,34 @@
+{ pkgs, ... }:
+
+{
+  config = let
+    user = "encryption_manager";
+  in {
+    users.users."${user}" = {
+      isNormalUser = false;
+      isSystemUser = true;
+    };
+
+    systemd.services = let
+      encryption_manager = pkgs.callPackage (pkgs.fetchFromGitHub {
+        owner  = "msf-ocb";
+        repo   = "nixos_encryption_manager";
+        rev    = "81e4ab6b040d160505c06797a824bd7cdb233b7e";
+        sha256 = "1vyxnrw4kylxkrpkiqy13knbpcia3v05nb4k832w3jf173al89xl";
+      }) {};
+    in {
+      data_manager = {
+        description   = "";
+        serviceConfig = {
+          User    = user;
+          Type    = "simple";
+          Restart = "always";
+        };
+        script        = ''
+          ${data_manager}/bin/nixos_encryption_manager
+        '';
+      };
+    };
+  };
+}
+
