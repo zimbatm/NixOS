c36108bbe6d04c17207124669d47655cd428880e
R-VdP <141248+R-VdP@users.noreply.github.com>
2020-05-04 00:51:52 +0200

    Clean up the deploy script.

--- .github/actions/nixos_deploy/deploy.sh
+++ .github/actions/nixos_deploy/deploy.sh
@@ -1,21 +1,23 @@
 #! /bin/sh
 
-umask 0077
+umask 0377
 
-# The workdir of the Docker container will be the clone of the NixOS repo
 ANSIBLE_DIR="/nixos_deploy"
 
 KEYFILE="/root/.id_ec"
 VAULTPASS="/root/.vault_pass"
+HOSTFILE="${ANSIBLE_DIR}/hosts.yml"
 CONNECTION_TIMEOUT=120
 
 echo "${VAULT_PASS}" > "${VAULTPASS}"
-ansible-vault view --vault-password-file="${VAULTPASS}" "${ANSIBLE_DIR}"/id_ec_robot.secret > "${KEYFILE}"
-chmod 0400 "${KEYFILE}"
+ansible-vault view --vault-password-file="${VAULTPASS}" \
+                   "${ANSIBLE_DIR}"/id_ec_robot.secret \
+                   > "${KEYFILE}"
 
 python3 "${ANSIBLE_DIR}"/build_inventory.py --keyfile "${KEYFILE}" \
                                             --timeout "${CONNECTION_TIMEOUT}" \
-                                            --eventlog "${GITHUB_EVENT_PATH}" > hosts.yml
+                                            --eventlog "${GITHUB_EVENT_PATH}" \
+                                            > "${HOSTFILE}"
 
 export ANSIBLE_PYTHON_INTERPRETER="auto_silent"
 export ANSIBLE_HOST_KEY_CHECKING="False"
@@ -23,7 +25,6 @@ export ANSIBLE_SSH_RETRIES=5
 ansible-playbook --timeout="${CONNECTION_TIMEOUT}" \
                  --key-file "${KEYFILE}" \
                  --vault-password-file "${VAULTPASS}" \
-                 --inventory hosts.yml \
-                 --extra-vars "build_sha=${GITHUB_SHA}" \
+                 --inventory "${HOSTFILE}" \
                  "${ANSIBLE_DIR}"/deploy.yml
 
