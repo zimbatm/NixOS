c35c9a521ced11b8487d4b9d8368fca30cf12d30
R-VdP <141248+R-VdP@users.noreply.github.com>
2020-05-04 00:59:34 +0200

    Revert "Clean up the deploy script."
    This reverts commit fb16bf08f4fe9ff4eb2217776a40d07d4feb6349.

--- .github/actions/nixos_deploy/deploy.sh
+++ .github/actions/nixos_deploy/deploy.sh
@@ -1,23 +1,21 @@
 #! /bin/sh
 
-umask 0377
+umask 0077
 
+# The workdir of the Docker container will be the clone of the NixOS repo
 ANSIBLE_DIR="/nixos_deploy"
 
 KEYFILE="/root/.id_ec"
 VAULTPASS="/root/.vault_pass"
-HOSTFILE="${ANSIBLE_DIR}/hosts.yml"
 CONNECTION_TIMEOUT=120
 
 echo "${VAULT_PASS}" > "${VAULTPASS}"
-ansible-vault view --vault-password-file="${VAULTPASS}" \
-                   "${ANSIBLE_DIR}"/id_ec_robot.secret \
-                   > "${KEYFILE}"
+ansible-vault view --vault-password-file="${VAULTPASS}" "${ANSIBLE_DIR}"/id_ec_robot.secret > "${KEYFILE}"
+chmod 0400 "${KEYFILE}"
 
 python3 "${ANSIBLE_DIR}"/build_inventory.py --keyfile "${KEYFILE}" \
                                             --timeout "${CONNECTION_TIMEOUT}" \
-                                            --eventlog "${GITHUB_EVENT_PATH}" \
-                                            > "${HOSTFILE}"
+                                            --eventlog "${GITHUB_EVENT_PATH}" > hosts.yml
 
 export ANSIBLE_PYTHON_INTERPRETER="auto_silent"
 export ANSIBLE_HOST_KEY_CHECKING="False"
@@ -25,6 +23,7 @@ export ANSIBLE_SSH_RETRIES=5
 ansible-playbook --timeout="${CONNECTION_TIMEOUT}" \
                  --key-file "${KEYFILE}" \
                  --vault-password-file "${VAULTPASS}" \
-                 --inventory "${HOSTFILE}" \
+                 --inventory hosts.yml \
+                 --extra-vars "build_sha=${GITHUB_SHA}" \
                  "${ANSIBLE_DIR}"/deploy.yml
 
