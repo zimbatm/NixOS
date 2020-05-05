0fabb9706c2c0fe0d827fc8070204a954718adee
R-VdP <141248+R-VdP@users.noreply.github.com>
2020-05-05 11:22:26 +0200

    POXIS-compliant lower-case variable names.

--- .github/actions/nixos_deploy/deploy.sh
+++ .github/actions/nixos_deploy/deploy.sh
@@ -2,26 +2,26 @@
 
 umask 0077
 
-ANSIBLE_DIR="/nixos_deploy"
-KEYFILE="/root/.id_ec"
-VAULTPASS="/root/.vault_pass"
-HOSTFILE="${ANSIBLE_DIR}/hosts.yml"
-CONNECTION_TIMEOUT=120
+ansible_dir="/nixos_deploy"
+keyfile="/root/.id_ec"
+vaultpass="/root/.vault_pass"
+hostfile="${ansible_dir}/hosts.yml"
+connection_timeout=120
 
-echo "${VAULT_PASS}" > "${VAULTPASS}"
-ansible-vault view --vault-password-file="${VAULTPASS}" "${ANSIBLE_DIR}"/id_ec_robot.secret > "${KEYFILE}"
-chmod 0400 "${KEYFILE}"
+echo "${VAULT_PASS}" > "${vaultpass}"
+ansible-vault view --vault-password-file="${vaultpass}" "${ansible_dir}"/id_ec_robot.secret > "${keyfile}"
+chmod 0400 "${keyfile}"
 
-python3 "${ANSIBLE_DIR}"/build_inventory.py --keyfile "${KEYFILE}" \
-                                            --timeout "${CONNECTION_TIMEOUT}" \
-                                            --eventlog "${GITHUB_EVENT_PATH}" > "${HOSTFILE}"
+python3 "${ansible_dir}"/build_inventory.py --keyfile "${keyfile}" \
+                                            --timeout "${connection_timeout}" \
+                                            --eventlog "${GITHUB_EVENT_PATH}" > "${hostfile}"
 
 export ANSIBLE_PYTHON_INTERPRETER="auto_silent"
 export ANSIBLE_HOST_KEY_CHECKING="False"
 export ANSIBLE_SSH_RETRIES=5
-ansible-playbook --timeout="${CONNECTION_TIMEOUT}" \
-                 --key-file "${KEYFILE}" \
-                 --vault-password-file "${VAULTPASS}" \
-                 --inventory "${HOSTFILE}" \
-                 "${ANSIBLE_DIR}"/deploy.yml
+ansible-playbook --timeout="${connection_timeout}" \
+                 --key-file "${keyfile}" \
+                 --vault-password-file "${vaultpass}" \
+                 --inventory "${hostfile}" \
+                 "${ansible_dir}"/deploy.yml
 
