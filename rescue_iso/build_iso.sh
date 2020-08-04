71f8ba13951b39de14c5624603504337d964f607
R-VdP <141248+R-VdP@users.noreply.github.com>
2020-08-04 11:09:58 +0200

    Allow the build_iso script to be called from outside the rescue_iso directory.

--- rescue_iso/build_iso.sh
+++ rescue_iso/build_iso.sh
@@ -1,6 +1,9 @@
 #! /usr/bin/env bash
 
-nix-build '<nixpkgs/nixos>' -A config.system.build.isoImage -I nixos-config=iso.nix
+iso_dir="$(dirname "${BASH_SOURCE}")"
+
+nix-build '<nixpkgs/nixos>' -A config.system.build.isoImage \
+                            -I nixos-config="${iso_dir}/iso.nix"
 
 if [ "${?}" -eq "0" ]; then
   echo -e "\nThe ISO can be found in result/iso.\n"
