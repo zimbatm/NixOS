aaef86f6278e444e93bc2535ae08154bed64a0c7
R-VdP <141248+R-VdP@users.noreply.github.com>
2020-08-11 01:03:30 +0200

    No need to run cachix as root when specifying the -m flag.

--- scripts/configure_cachix.sh
+++ scripts/configure_cachix.sh
@@ -3,8 +3,5 @@
 
 modules_dir="$(dirname "${BASH_SOURCE}")/../modules/"
 
-sudo cachix use -d "${modules_dir}" panic-button
-
-mode="$(id -nu):$(id -ng)"
-sudo chown -R "${mode}" "${modules_dir}/cachix/" "${modules_dir}/cachix.nix"
+cachix use -m nixos -d "${modules_dir}" panic-button
 
