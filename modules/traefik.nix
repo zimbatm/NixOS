7c10c3b8f89149ee1bb22c083a0412219dcba5a2
R-VdP <141248+R-VdP@users.noreply.github.com>
2020-08-20 08:18:46 +0200

    Disable 128bit ciphers.

--- modules/traefik.nix
+++ modules/traefik.nix
@@ -118,12 +118,10 @@ in
               minVersion: "VersionTLS12"
               sniStrict: true
               cipherSuites:
-                - "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256"
-                - "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384"
                 - "TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305"
-                - "TLS_AES_128_GCM_SHA256"
-                - "TLS_AES_256_GCM_SHA384"
+                - "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384"
                 - "TLS_CHACHA20_POLY1305_SHA256"
+                - "TLS_AES_256_GCM_SHA384"
       '';
     in {
       "${cfg.service_name}" = {
