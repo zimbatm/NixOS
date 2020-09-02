b1759e4e915cf65f8feb85b6d4dc72b63fbd7a93
R-VdP <141248+R-VdP@users.noreply.github.com>
2020-09-02 17:25:02 +0200

    Print the key to be added to github to the screen.

--- install.sh
+++ install.sh
@@ -164,7 +164,8 @@ fi
     echo -e "\nThe SSH key in /tmp/id_tunnel, does not give us access to GitHub."
     echo    "Please add the public key (/tmp/id_tunnel.pub) to"
     echo    "the tunnels.json file in the NixOS-OCB-config repo."
-    echo    "To view the key and copy it, run: \"cat /tmp/id_tunnel.pub\""
+    echo -e "I am printing below the key that you should copy and put on GitHub:\n"
+    cat /tmp/id_tunnel.pub
     echo -e "\nYou can restart the installer once this is done and"
     echo -e "the GitHub deployment actions have run.\n"
     echo    "If you want me to generate a new key pair instead,"
