3699f333ceb02b8c4bdf19622654b1f98653c7df
R-VdP <141248+R-VdP@users.noreply.github.com>
2020-09-11 00:51:59 +0200

    [Traefik] Request EC256 certificates.

--- modules/traefik.nix
+++ modules/traefik.nix
@@ -99,6 +99,7 @@ in
             acme:
               email: ${cfg.acme.email_address}
               storage: ${cfg.acme.storage}/acme.json
+              keyType: EC256
               #caserver: http://acme-staging-v02.api.letsencrypt.org/directory
               httpchallenge:
                 entrypoint: web
