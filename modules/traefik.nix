946f0edfa4b85fad490e50ccad6ad4f0a6c4cf8c
Ramses <141248+R-VdP@users.noreply.github.com>
2020-09-14 15:55:28 +0000

    [Traefik] Enable TLS 1.2 ECDSA cipher suites.

--- modules/traefik.nix
+++ modules/traefik.nix
@@ -133,7 +133,9 @@ in
               minVersion: "VersionTLS12"
               sniStrict: true
               cipherSuites:
+                - "TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256"
                 - "TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305"
+                - "TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384"
                 - "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384"
                 - "TLS_CHACHA20_POLY1305_SHA256"
                 - "TLS_AES_256_GCM_SHA384"
