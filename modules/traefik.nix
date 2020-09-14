d7367670c9d4ad7f299cab84903e064d416416e3
Ramses <141248+R-VdP@users.noreply.github.com>
2020-09-14 16:19:57 +0000

    [Traefik] Change legacy name to include suffix, add documentation, prefer AES for TLS 1.3.

--- modules/traefik.nix
+++ modules/traefik.nix
@@ -133,12 +133,15 @@ in
               minVersion: "VersionTLS12"
               sniStrict: true
               cipherSuites:
+                # https://godoc.org/crypto/tls#pkg-constants
+                # TLS 1.2
                 - "TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256"
-                - "TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305"
+                - "TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256"
                 - "TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384"
                 - "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384"
-                - "TLS_CHACHA20_POLY1305_SHA256"
+                # TLS 1.3
                 - "TLS_AES_256_GCM_SHA384"
+                - "TLS_CHACHA20_POLY1305_SHA256"
       '';
     in {
       "${cfg.service_name}" = {
