3de3f467bcb13052a060cdd658993a3e53bbcac3
R-VdP <141248+R-VdP@users.noreply.github.com>
2020-09-28 12:13:57 +0200

    Prefer AES over CHACHA and disable CTR modes.

--- modules/sshd.nix
+++ modules/sshd.nix
@@ -38,6 +38,8 @@ with lib;
         kexAlgorithms = [ "curve25519-sha256@libssh.org"
                           "diffie-hellman-group18-sha512"
                           "diffie-hellman-group16-sha512" ];
+        ciphers = [ "aes256-gcm@openssh.com"
+                    "chacha20-poly1305@openssh.com" ];
         macs = [ "hmac-sha2-512-etm@openssh.com"
                  "hmac-sha2-256-etm@openssh.com"
                  "umac-128-etm@openssh.com" ];
