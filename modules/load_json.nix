221a1e8a9a3561c0ce23cf1579f23c9b40c31f3f
R-VdP <141248+R-VdP@users.noreply.github.com>
2020-10-27 12:28:55 +0100

    Refactor the load_json module to be better structured.

--- modules/load_json.nix
+++ modules/load_json.nix
@@ -5,9 +5,9 @@ with (import ../msf_lib.nix);
 
 {
   config = let
-    sys_cfg   = config.settings.system;
-    host_name = config.settings.network.host_name;
-    loadJSON  = msf_lib.compose [ builtins.fromJSON builtins.readFile ];
+    sys_cfg  = config.settings.system;
+    hostName = config.settings.network.host_name;
+    loadJSON = msf_lib.compose [ builtins.fromJSON builtins.readFile ];
   in {
     settings = {
       users.users = let
@@ -18,30 +18,53 @@ with (import ../msf_lib.nix);
         # Load the list at path in an attribute set and convert it to
         # an attribute set with every list element as a key and the value
         # set to a given constant.
-        # If the given path cannot be found, the value of onAbsent will be returned.
+        # If the given path cannot be found in the loaded JSON structure,
+        # then the value of onAbsent will be used as input instead.
+        #
         # Example:
-        #   listToAttrs_const [ "per-host" "benuc002" "enable" ] val [] { per-host.benuc002.enable = [ "foo", "bar" ]; }
+        #   listToAttrs_const [ "per-host" "benuc002" "enable" ]
+        #                     val
+        #                     []
+        #                     { per-host.benuc002.enable = [ "foo", "bar" ]; }
         # will yield:
         #   { foo = val; bar = val; }
-        listToAttrs_const = path: const: onAbsent: msf_lib.compose [ (flip genAttrs (_: const))
-                                                                     (attrByPath path onAbsent) ];
+        listToAttrs_const = path: const: onAbsent:
+          msf_lib.compose [ (flip genAttrs (_: const))
+                            (attrByPath path onAbsent) ];
 
         # recursiveUpdate merges the two resulting attribute sets recursively
         recursiveMerge = foldr recursiveUpdate {};
-        # Given the host name and the json data, retrieve the enabled roles for the given host
-        enabledRoles   = host_name: attrByPath [ "users" "per-host" host_name "enable_roles" ] [];
-        onRoleAbsent   = role: host_name: abort ''The role "${role}" which was enabled for host "${host_name}" is not defined.'';
-      in
-        recursiveMerge ([ (listToAttrs_const [ "users" "remote_tunnel" ]               remoteTunnel       [] json_data)
-                          (listToAttrs_const [ "users" "per-host" host_name "enable" ] { enable = true; } [] json_data) ] ++
-                          (map (role: listToAttrs_const [ "users" "roles" role ] { enable = true; } (onRoleAbsent role host_name) json_data)
-                               (enabledRoles host_name json_data)));
+
+        remoteTunnelUsers = listToAttrs_const [ "users" "remote_tunnel" ]
+                                              remoteTunnel
+                                              [] json_data;
+        enabledUsers      = listToAttrs_const [ "users" "per-host" hostName "enable" ]
+                                              { enable = true; }
+                                              [] json_data;
+
+        enabledUsersByRoles = let
+          # Given the host name and the json data,
+          # retrieve the enabled roles for the given host
+          enabledRoles = hostName:
+            attrByPath [ "users" "per-host" hostName "enable_roles" ] [];
+          onRoleAbsent = role: hostName:
+            abort ''The role "${role}" which was enabled for host "${hostName}" is not defined.'';
+          # Activate the users in the given role
+          activateRole = hostName: role:
+            listToAttrs_const [ "users" "roles" role ]
+                              { enable = true; }
+                              (onRoleAbsent role hostName)
+                              json_data;
+          activateRoles = hostName: map (activateRole hostName);
+        in activateRoles hostName (enabledRoles hostName json_data);
+      in recursiveMerge ([ remoteTunnelUsers
+                           enabledUsers ] ++
+                         enabledUsersByRoles);
 
       reverse_tunnel.tunnels = let
         tunnel_json_path = sys_cfg.tunnels_json_path;
         json_data        = loadJSON tunnel_json_path;
-      in
-        json_data.tunnels.per-host;
+      in json_data.tunnels.per-host;
     };
   };
 }
