303b8c34f08f22652223ceb6434efa5c9ddc83a2
R-VdP <141248+R-VdP@users.noreply.github.com>
2020-11-07 18:04:33 +0100

    Check for the presence of the right device labels on building to avoid unbootable systems.

--- configuration.nix
+++ configuration.nix
@@ -2,6 +2,16 @@
 
 with lib;
 
+let
+  root_device = "/dev/disk/by-label/nixos_root";
+  boot_device = "/dev/disk/by-label/nixos_boot";
+  efi_device  = "/dev/disk/by-label/EFI";
+
+  boot_cfg = config.settings.boot;
+  boot_device_enabled = boot_cfg.separate_partition;
+  efi_device_enabled  = boot_cfg.mode == boot_cfg.modes.uefi;
+in
+
 {
   imports = [
     ./hardware-configuration.nix
@@ -9,22 +19,39 @@ with lib;
     ./modules
   ];
 
+  # Print a warning when the correct labels are not present to avoid
+  # an unbootable system.
+  warnings = let
+    mkWarningMsg = name: path:
+      "The ${name} partition is not correctly labelled! " +
+      "This installation will probably not boot!\n" +
+      "Missing path: ${path}";
+    labelCondition = path: ! builtins.pathExists path;
+    mkWarning = name: path: enable: optional (enable && labelCondition path)
+                                             (mkWarningMsg name path);
+
+    root_warnings = mkWarning "root" root_device true;
+    boot_warnings = mkWarning "boot" boot_device boot_device_enabled;
+    efi_warnings  = mkWarning "efi"  efi_device  efi_device_enabled;
+  in
+    root_warnings ++ boot_warnings ++ efi_warnings;
+
   # We need to force to override the definition in the default AWS config.
   fileSystems = mkForce {
     "/" = {
-      device  = "/dev/disk/by-label/nixos_root";
+      device  = root_device;
       fsType  = "ext4";
       options = [ "defaults" "noatime" "acl" ];
       autoResize = true;
     };
-    "/boot" = mkIf config.settings.boot.separate_partition {
-      device  = "/dev/disk/by-label/nixos_boot";
+    "/boot" = mkIf boot_device_enabled {
+      device  = boot_device;
       fsType  = "ext4";
       options = [ "defaults" "noatime" "nosuid" "nodev" "noexec" ];
       autoResize = true;
     };
-    "/boot/efi" = mkIf (config.settings.boot.mode == "uefi") {
-      device = "/dev/disk/by-label/EFI";
+    "/boot/efi" = mkIf efi_device_enabled {
+      device = efi_device;
       fsType = "vfat";
     };
   };
