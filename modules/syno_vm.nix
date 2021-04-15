979195d111ae7a79e61a7aca13db77b2473ad7cd
Ramses <141248+R-VdP@users.noreply.github.com>
2021-04-15 15:26:31 +0200

    Add a module for synology VMs. (#58)

---
+++ modules/syno_vm.nix
@@ -0,0 +1,20 @@
+{ config, lib, ...}:
+
+let
+  cfg = config.settings.syno_vm;
+in
+
+with lib;
+
+{
+  options.settings.syno_vm = {
+    enable = mkEnableOption "the QEMU guest services for Synology VMs";
+  };
+
+  config = mkIf cfg.enable {
+    # https://github.com/NixOS/nixpkgs/issues/91300
+    virtualisation.hypervGuest.enable = mkForce false;
+    services.qemuGuest.enable = true;
+  };
+}
+
