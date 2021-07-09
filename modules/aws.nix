a13ce98b8345a04dfa848c7ad50699d7afcade7b
Ramses <141248+R-VdP@users.noreply.github.com>
2021-07-09 14:42:22 +0200

    Do not perform an upgrade during the day for AWS machines. (#110)

--- modules/aws.nix
+++ modules/aws.nix
@@ -8,7 +8,14 @@ with lib;
   ];
 
   ec2.hvm = true;
-  settings.boot.mode = "none";
+  settings = {
+    boot.mode = "none";
+    # For our AWS servers, we do not need to run the upgrade service during
+    # the day.
+    # Upgrading during the day can cause the nixos_rebuild_config service to
+    # refuse to activate the new config due to an upgraded kernel.
+    maintenance.nixos_upgrade.startAt = [ "Mon 02:00" ];
+  };
   services.timesyncd.servers = config.networking.timeServers;
 
   networking.dhcpcd = {
