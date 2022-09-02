07e52f1ae8a02a728c3081af8b414eb6918eac03
Ramses <141248+R-VdP@users.noreply.github.com>
2022-09-02 15:33:43 +0200

    Simplify configuration.nix. (#257)

--- configuration.nix
+++ configuration.nix
@@ -1,8 +1,5 @@
-{ config, ... }:
-
 # By default we import the settings.nix file which points to the host file for
 # the current server.
-
 {
   imports = [
     (import ./eval_host.nix { host_path = ./settings.nix; })
